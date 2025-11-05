3a290f7c3db6ce290b6ae4978ca42b54de8fa025
Vitor Eduardo <vitornascimento6650@gmail.com>
2025-11-04 23:34:14 -0300

    estrutura e estilizacao da pagina de login e mudanca no background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/mekhalogin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'mekhalogin',
+    component: LoginComponent,
+    title: 'Login'
+  }
+];
