9faf340f59005c542e78ebdfb07316e1a347a5b0
Vitor Eduardo <vitornascimento6650@gmail.com>
2025-11-06 18:52:05 -0300

    feat: create route for spin-products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login-component/login-component';
+import { InputOutputProductsComponent } from './pages/input-output-products-component/input-output-products-component';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,11 @@ export const routes: Routes = [
     path: 'mekhalogin',
     component: LoginComponent,
     title: 'Login'
+  },
+
+  {
+    path: 'spin-products',
+    component: InputOutputProductsComponent,
+    title: 'Entrada e Saida de produtos'
   }
 ];
